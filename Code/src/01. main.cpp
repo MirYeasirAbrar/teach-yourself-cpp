1894cab963e8b1396defe062e29791e4e99b3ffd
Mir Yeasir Abrar <abrar169@outlook.com>
2025-02-01 23:19:57 +0600

    code

---
+++ Code/src/01. main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main()
+{
+    std::cout << "I like MoMo!" << std::endl;
+    std::cout << "It's really good" << std::endl;
+    std::cout << "It's really very good" << '\n';
+
+    // This is a comment
+    /*
+    this
+    is a 
+    multi line 
+    comment
+    */
+
+    return 0;
+}
