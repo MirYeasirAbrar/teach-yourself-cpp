43825f26ba022a21df05dbca379593a3bdc51c4b
Mir Yeasir Abrar <abrar169@outlook.com>
2025-02-03 22:18:44 +0600

    Create 02. Variable.cpp

---
+++ Code/src/02. Variable.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main()
+{
+    int x; // Declaretion
+    x = 5;
+
+    int y = 6;
+    int sum = x + y;
+
+    /* std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n'; */
+
+    // Variable
+
+    // integer
+    int age = 22;
+
+    // double
+    double price = 21.234;
+
+    // string charecter
+    char currency = '$';
+    // std::cout << currency << '\n';
+
+    // boolean (true or false)
+    bool student = true;
+
+    // string (object that repressents a sequence of a text)
+    std::string name = "Mir Yeasir Abrar";
+
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old" << '\n';
+
+
+    return 0;
+}
